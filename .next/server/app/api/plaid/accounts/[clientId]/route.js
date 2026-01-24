e6e80de672822268028e484da46262a169f4a43d
Faran Zafar <farandeadz@gmail.com>
2026-01-24 23:23:28 +0000

    Stop tracking .next build output

--- .next/server/app/api/plaid/accounts/[clientId]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../../chunks/[turbopack]_runtime.js")("server/app/api/plaid/accounts/[clientId]/route.js")
-R.c("server/chunks/[root-of-the-server]__52e060c3._.js")
-R.c("server/chunks/node_modules_2fbcf8cf._.js")
-R.m("[project]/.next-internal/server/app/api/plaid/accounts/[clientId]/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/plaid/accounts/[clientId]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/plaid/accounts/[clientId]/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/static/chunks/src_app_accountant_page_tsx_2d7517cd._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_2d8d316d._.js"
-],
-    source: "dynamic"
-});
